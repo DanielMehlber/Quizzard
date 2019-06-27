362b8a36f66fd468cd3c2ece04c4fa52c7e93243
warnberger <warnberger@RR2-27.gym-friedberg.de>
2019-06-27 11:57:18 +0200

    Added function Disconnect Added documentation

--- quizzard/src/database/Connection.java
+++ quizzard/src/database/Connection.java
@@ -3,10 +3,27 @@ package database;
 import java.sql.*;
 
 import main.Console;
+import main.MainComponent;
 
+/**
+ * Connection between program and database
+ * @author warnberger
+ */
 public class Connection {
 	private java.sql.Connection con;
+	private Statement stm;
 	
+	/**
+	 * Creates class Connection
+	 * @param m MainComponent
+	 */
+	public Connection (MainComponent m) {
+		MainComponent main = m;
+	}
+	
+	/**
+	 * Connect the program with SQL database and creates a statement.
+	 */
 	public void connect() {
 		try {
 			Class.forName("org.postgresql.Driver");
@@ -19,8 +36,28 @@ public class Connection {
 			Console.error("database", "Cannot load driver", true);
 			e.printStackTrace();
 		}
+		
+		try {
+			stm=con.createStatement();
+		} catch (SQLException e) {
+			Console.error("database", "Connot creat statement", true);
+			e.printStackTrace();
+		}
+		
 		Console.info("database", "Connected", false);
 		
 		
+	}
+	
+	/**
+	 * Disconnects the program from the database.
+	 */
+	public void disconnect () {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			Console.error("database", "Connot disconnect", true);
+			e.printStackTrace();
+		}
 	}
 }
