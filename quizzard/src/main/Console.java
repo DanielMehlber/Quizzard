fd3d61e279058cd7d852a01451f41a5e22e51087
warnberger <warnberger@RR2-27.gym-friedberg.de>
2019-06-27 11:43:49 +0200

    Console

--- quizzard/src/main/Console.java
+++ quizzard/src/main/Console.java
@@ -7,13 +7,13 @@ public class Console {
 	public static void info(String title, String message, boolean popup) {
 		System.out.println("INFO: <"+title+">: "+message);
 		if(popup)
-			JOptionPane.showMessageDialog(null, title, message, JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
 	}
 	
 	public static void error(String title, String message, boolean popup) {
-		System.err.println("INFO: <"+title+">: "+message);
+		System.err.println("ERROR: <"+title+">: "+message);
 		if(popup)
-			JOptionPane.showMessageDialog(null, title, message, JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, message, title, JOptionPane.ERROR_MESSAGE);
 	}
 
 }
