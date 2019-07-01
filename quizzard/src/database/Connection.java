05eccc59029c2285cacdcf052d736b8ccf4d3f8f
warnberger <warnberger@RR2-27.gym-friedberg.de>
2019-07-01 10:14:53 +0200

    .

--- quizzard/src/database/Connection.java
+++ quizzard/src/database/Connection.java
@@ -70,37 +70,28 @@ public class Connection {
 	 * @return Code arrors
 	 */
 	public ErrCode login (String name, String pw) {
-		ArrayList<String> n= new ArrayList<String>();
-		ArrayList<String> p= new ArrayList<String>();
 		ErrCode code = ErrCode.NULL;
 		//Hashcode
 		//pw=hashcode(pw);
 		try {
 			ResultSet rs=stm.executeQuery("SELECT name FROM q11info1.Player WHERE (name='"+name+"');");
-			while (rs.next()) {
-				n.add(rs.getString("name"));
+			if(rs.next()==false) {
+				code=ErrCode.ERR_LOGIN_UNKNOWN_USER;
 			}
 			
-			for(String s : n) {
-				System.out.println(s);
-			}
+
 			
 					
 		} catch (SQLException e) {
 			Console.error("databank", "Cannot found Player", false);
-			code=ErrCode.ERR_LOGIN_UNKNOWN_USER;
 			e.printStackTrace();
 		}
 		try {
 			rs=stm.executeQuery("SELECT password FROM q11info1.Player WHERE (name='"+name+"' AND password='"+pw+"');");
-			while (rs.next()) {
-				p.add(rs.getString("password"));
-			}
-			for(String s : p) {
-				System.out.println(s);
+			if (rs.next()==false) {
+				code= ErrCode.ERR_LOGIN_INCORRECT_PASSWORD;
 			}
 		} catch(SQLException e) {
-			code= ErrCode.ERR_LOGIN_INCORRECT_PASSWORD;
 			Console.error("databank", "Incorrect password", false);
 		}
 
