b49133eb84427dcaa4aed69af05bf5c18facb2f5
hanne <hanne@DESKTOP-6FAJ6NM.fritz.box>
2019-07-18 14:24:08 +0200

    GameDataFinished

--- quizzard/src/main/GameData.java
+++ quizzard/src/main/GameData.java
@@ -3,26 +3,32 @@ package main;
 public class GameData {
 
 	private int id;
-	private String name;
-	private int[] player;
+	private boolean öffentlich;
+	private int[] playerid;
+	private String password;
+	private String describtion;
 	private int round;
 	private int max_rounds;
 	
-	public GameData(int id, String name, int[] player, int i, int j) {
+	public GameData(int id, int[] p, String pw, String de, int i, int j) {
 		this.id = id;
-		this.name = name;
-		this.player = player;
+		this.playerid = p;
+		password=pw;
+		describtion=de;
 		this.round = i;
 		this.max_rounds = j;
 	}
 	public void setId(int i) {
 		id=i;
 	}
-	public void setName(String n) {
-		name=n;
+	public void setPw(String n) {
+		password=n;
+	}
+	public void setDes(String d) {
+		describtion=d;
 	}
 	public void setPlayers (int[] p) {
-		player=p;
+		playerid=p;
 	}
 	public void setI(int a) {
 		round=a;
